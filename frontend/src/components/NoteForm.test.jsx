ef0f7754ade6a9201ba13f3425482eaa86792c54
vrahkonenn <vrahkonen02@gmail.com>
2025-10-17 19:10:26 +0300

    Added few frontend tests

---
+++ frontend/src/components/NoteForm.test.jsx
@@ -0,0 +1,19 @@
+import { render, screen } from '@testing-library/react'
+import NoteForm from './NoteForm'
+import userEvent from '@testing-library/user-event'
+
+test('<NoteForm /> updates parent state and calls onSubmit', async () => {
+  const createNote = vi.fn()
+
+  render(<NoteForm createNote={createNote} />) 
+
+
+  const input = screen.getByPlaceholderText('write note content here')
+  const sendButton = screen.getByText('save')
+
+  await userEvent.type(input, 'testing a form...' )
+  await userEvent.click(sendButton)
+
+  expect(createNote.mock.calls).toHaveLength(1)
+  expect(createNote.mock.calls[0][0].content).toBe('testing a form...' )
+})
